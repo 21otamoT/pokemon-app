ed4f46c0d2e8f42aab373b6cb6442a26c55acbb1
hirose <yukihio76@gmail.com>
2024-03-19 18:07:24 +0900

    feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import {useEffect, useState} from 'react'
-import './App.css';
-import './component.css'
-import { getAllPokemon, getPokemon } from './utils/pokemon';
-import Card from './components/Card/Card';
-import Navbar from './components/Navbar/Navbar';
+import { useEffect, useState } from "react";
+import "./App.css";
+import "./component.css";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
+import ReactPaginate from "react-paginate";
 
 function App() {
-  const initialURL = 'https://pokeapi.co/api/v2/pokemon?limit=39';
+  const initialURL = "https://pokeapi.co/api/v2/pokemon?limit=1008";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
-  const [nextURL, setNextURL] = useState('');
-  const [prevURL, setPrevURL] = useState('');
+  const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 30;
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = pokemonData.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(pokemonData.length / itemsPerPage);
+
+  const handlePageClick = (e) => {
+    const newOffset = (e.selected * itemsPerPage) % pokemonData.length;
+    setItemOffset(newOffset);
+  };
 
   useEffect(() => {
     const feachPokemonData = async () => {
@@ -24,15 +35,15 @@ function App() {
       setLoading(false);
     };
     feachPokemonData();
-  }, [])
+  }, []);
 
   const loadPokemon = async (data) => {
     let _pokemonData = await Promise.all(
-      data.map(pokemon => {
+      data.map((pokemon) => {
         let pokemonRecord = getPokemon(pokemon.url);
         return pokemonRecord;
       })
-    )
+    );
     setPokemonData(_pokemonData);
   };
   // console.log(pokemonData);
@@ -42,13 +53,13 @@ function App() {
     let data = await getAllPokemon(nextURL);
     // console.log(data);
     await loadPokemon(data.results);
-    setNextURL(data.next)
+    setNextURL(data.next);
     setPrevURL(data.previous);
     setLoading(false);
-  }
+  };
 
   const handlePrevPage = async () => {
-    if(!prevURL) return;
+    if (!prevURL) return;
 
     setLoading(true);
     let data = await getAllPokemon(prevURL);
@@ -56,7 +67,7 @@ function App() {
     setNextURL(data.next);
     setPrevURL(data.previous);
     setLoading(false);
-  }
+  };
 
   return (
     <>
@@ -66,14 +77,31 @@ function App() {
           <h1>ローディング中・・・</h1>
         ) : (
           <>
-            <div className='pokemonCardContainer'>
-              {pokemonData.map((pokemon, i) => {
-                return <Card key={i} pokemon={pokemon}/>;
-              })}
+            <div className="pokemonCardContainer">
+              {currentItems.map((pokemon, i) => (
+                <Card key={i} pokemon={pokemon} />
+              ))}
             </div>
-            <div className='btn'>
-              <button onClick={handlePrevPage} className='btn-blue'>前へ</button>
-              <button onClick={handleNextPage} className='btn-blue'>次へ</button>
+            <div className="btn">
+              <ReactPaginate
+                pageCount={pageCount}
+                nextLabel="次"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                previousLabel="前"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+              />
             </div>
           </>
         )}
